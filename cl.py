950a784105581e71afbafa75de29f9e3fbc9af66
Lzboy17 <axmedxalid2@gmail.com>
2021-10-31 13:03:06 +0200

    Create cl.py

---
+++ cl.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#*code:UTF-8*
+B="\033[0;30m" # Black
+R="\033[1;31m" # Red
+G="\033[1;32m" # Green
+Y="\033[1;33m" # Yellow
+Bl="\033[1;34m" # Blue
+P="\033[1;35m" # Purple
+C="\033[1;36m" # Cyan
+W="\033[1;37m" # Whit
+Af="\033[1m"
+Bf="\033[0m" 
+###############
+ok=(G+"*"+W)           
+am=(Bl+">> "+W)          
+no=(R+"! "+W) 
